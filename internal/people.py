da847359cda56081e3f14af0da0f882d79497471
zefani <naruto3dd@gmail.com>
2022-09-08 14:26:51 +0300

    init

---
+++ internal/people.py
@@ -0,0 +1,9 @@
+class Client:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+class payer:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
